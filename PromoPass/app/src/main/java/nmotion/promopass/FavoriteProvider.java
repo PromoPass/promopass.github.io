9df28fde859de627f015ae8e1d1cb0e2cdfd71db
MsAwesome <jmcovington1@crimson.ua.edu>
2015-11-30 22:54:20 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	PromoPass/app/src/main/java/nmotion/promopass/ListNearbyProviders.java
    #	PromoPass/app/src/main/java/nmotion/promopass/ViewAd.java

--- PromoPass/app/src/main/java/nmotion/promopass/FavoriteProvider.java
+++ PromoPass/app/src/main/java/nmotion/promopass/FavoriteProvider.java
@@ -8,9 +8,13 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class FavoriteProvider extends AppCompatActivity {
-    private ListView nearbyProvidersView;
-    private ArrayAdapter<ReceivedAd> nearbyProviders;
+    private ListView ProviderListView;
+    private ArrayAdapter<String> ProviderList;
     private Toolbar toolbar;
 
     @Override
@@ -18,13 +22,44 @@ public class FavoriteProvider extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_provider);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        nearbyProvidersView = (ListView) findViewById(R.id.nearbyProviders_list);
-        nearbyProviders = new ArrayAdapter<ReceivedAd>(this, android.R.layout.simple_list_item_1);
-        nearbyProvidersView.setAdapter(nearbyProviders);
+        ProviderListView = (ListView) findViewById(R.id.favoriteProvider);
+        ProviderList = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        ProviderListView.setAdapter(ProviderList);
 
+        String consumerID = DeviceIdentifier.id(this); //7 for this emulator, 7 isnt seen
+        consumerID="22"; //Hard coded for the emulator
+
+        JSONArray providerArray = Reader.getResults("http://fendatr.com/api/v1/consumer/" + consumerID + "/received"); //change this to access the preferences and not received Ad
+
+        JSONObject jsonTemp;
+
+        try {
+            for (int i = 0; i < providerArray.length(); i++) {
+                jsonTemp = providerArray.getJSONObject(i);
+                String businessID = jsonTemp.getString("BusinessID");
+
+                JSONArray allArrays_JSONARRAY = Reader.getResults("http://fendatr.com/api/v1/business/" + businessID + "/name");
+
+                JSONObject jsonTemp2 = allArrays_JSONARRAY.getJSONObject(0);
+                /*
+                ReceivedAd favoriteProvider = new ReceivedAd(jsonTemp.getString("ReceivedAdID"),
+                        jsonTemp.getString("AdID"),
+                        businessID,
+                        jsonTemp2.getString("Name"),
+                        consumerID);
+                 */
+
+                ProviderList.add(providerArray.getString(i));
+
+
+
+               // seeReceivedAd(receivedAd.getReceivedAdID());
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
 
     }
